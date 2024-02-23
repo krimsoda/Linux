a491bee44225f0ba5c1a9b8d321af044992e4f93
krimsoda <160830222+krimsoda@users.noreply.github.com>
2024-02-23 23:16:17 +0800

    Update user_addgroup_sudo.sh

--- user_addgroup_sudo.sh
+++ user_addgroup_sudo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ###################################################################
 #Script Name	:addgroup                                                                                             
-#Description	:add group sudo RedHat: wheel Debian: sudo
-#Notes          :file: userlist.txt" change group sudo RedHat: wheel Debian: sudo                                                                
+#Description	:Add the User to the sudo Group: RedHat: wheel Debian: sudo
+#Notes          :file: userlist.txt"                                                               
 #Author       	:krimsoda                                               
 ###################################################################
 
